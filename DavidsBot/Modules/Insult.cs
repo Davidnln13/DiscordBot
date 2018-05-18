6f7d9562f5aebcdfb1f124163952256f488306e1
Davidnln13 <c00204958@itcarlow.ie>
2018-05-18 20:57:12 +0100

    moved Insult module to its own class

---
+++ DavidsBot/Modules/Insult.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavidsBot.Modules
+{
+    public class Insult : ModuleBase<SocketCommandContext>
+    {
+        [Command("insult_chris")]
+        public async Task FuckChrisAsync()
+        {
+            string insult = "";
+            Random r = new Random();
+            int num = r.Next(1, 6);
+            if (num == 1)
+            {
+                insult = "Go fuck yourself chris";
+            }
+            else if (num == 2)
+            {
+                insult = "chris you're a shit head";
+            }
+            else if (num == 3)
+            {
+                insult = "im chris i like asians wah";
+            }
+            else if (num == 4)
+            {
+                insult = "legend 3 btw";
+            }
+            else
+            {
+                insult = "ha i liek wocks what about you";
+            }
+            await ReplyAsync(insult);
+        }
+    }
+}
--- DavidsBot/Modules/helloworld.cs
+++ DavidsBot/Modules/helloworld.cs
@@ -14,33 +14,5 @@ namespace DavidsBot.Modules
             await ReplyAsync("Hi I am Davids bot whaddup fam");
         }
 
-        [Command("insult_chris")]
-        public async Task FuckChrisAsync()
-        {
-            string insult = "";
-            Random r = new Random();
-            int num = r.Next(1, 6);
-            if(num==1)
-            {
-                insult = "Go fuck yourself chris";
-            }
-            else if(num==2)
-            {
-                insult = "chris you're a shit head";
-            }
-            else if(num==3)
-            {
-                insult = "im chris i like asians wah";
-            }
-            else if(num==4)
-            {
-                insult = "legend 3 btw";
-            }
-            else
-            {
-                insult = "ha i liek wocks what about you";
-            }
-            await ReplyAsync(insult);
-        }
     }
 }
